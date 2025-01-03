f6e0c4c1482934cdc78e11eab01bb2ba41c98865
ramkrishna mondal <ramkrishnam170@gmail.com>
2025-01-03 20:33:19 +0530

    repested dna sequence done

---
+++ reopeated_dna_sequence.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        int n=s.length();
+        vector<string> ans;
+        if(n<10) return ans;
+        unordered_map<string,int> mp;
+        for(int i=0;i<n;i++){
+            string str=s.substr(i,10);
+            mp[str]++;
+        }
+
+        for(auto it:mp){
+            if(it.second>1){
+                ans.push_back(it.first);
+            }
+        }
+        return ans;
+    }
+};
